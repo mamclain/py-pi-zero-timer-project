55d2a220e61fe956d87049a0ab4d2fce438cb806
Mike Mclain <mamclain@gmail.com>
2019-10-11 05:27:07 -0400

    allow local network access to device

--- timer_project/run.py
+++ timer_project/run.py
@@ -372,4 +372,4 @@ def ajax_start_event(
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)  # run app in debug mode on port 5000
+    app.run(host="0.0.0.0", debug=True, port=5000)  # run app in debug mode on port 5000
